4e2809a5daa874e65c65027fe571e94c1116740e
shashank0711 <shashanksingh07011@gmail.com>
2024-07-09 03:44:08 +0530

    address screen added with functionality

---
+++ lib/features/personalization/screens/address screen/userAddressScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class userAddressScreen extends StatelessWidget {
+  const userAddressScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.cyan,
+        onPressed: () => ,
+      ),
+    );
+  }
+}
